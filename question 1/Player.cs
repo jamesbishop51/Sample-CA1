cfd7ed93079f6733895bce66d90a8cfe41d7a61f
James Bishop <S00187350@mail.itsligo.ie>
2019-10-24 11:50:30 +0100

    Add project files.

---
+++ question 1/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    public class Player
+    {
+
+        public int ID { get; }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+
+        public Player(int id, string playername, int score)
+        {
+            ID = id;
+            PlayerName = playername;
+            Score = score;
+        }
+    }
+}
---
+++ question 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1, "Millie", 0);
+            Player player2 = new Player(2, "Katie", 0);
+            Player player3 = new Player(3, "Kevin", 0);
+            Player player4 = new Player(4, "Conor", 0);
+            Player player5 = new Player(5, "Pauline", 0);
+
+
+
+        }
+    }
+}
